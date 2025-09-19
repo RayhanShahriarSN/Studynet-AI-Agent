6ceede012864fa7a062c7bb041393b543bcdaa72
Rayhan Shahriar <rayhanhbz1999@gmail.com>
2025-09-19 08:24:37 +0600

    requirements.txt Added

--- account/admin.py
+++ account/admin.py
@@ -5,14 +5,14 @@ from django.utils.translation import gettext_lazy as _
 
 class UserAdmin(BaseUserAdmin):
     # Fields to display in admin list view
-    list_display = ('id', 'username', 'email', 'is_admin', 'is_active')  # replaced 'name' with 'username'
+    list_display = ('id', 'username', 'email', 'is_admin', 'is_active','tokens_used')  # replaced 'name' with 'username'
     list_filter = ('is_admin', 'is_active')
     search_fields = ('username', 'email')
     ordering = ('id',)
 
-    # Fields for the add user form
+    # Fields for the add user/edit user
     fieldsets = (
-        (None, {'fields': ('username', 'email', 'password')}),
+        (None, {'fields': ('username', 'email', 'password', 'tokens_used')}),
         (_('Permissions'), {'fields': ('is_admin', 'is_active')}),
         (_('Important dates'), {'fields': ('last_login',)}),
     )
@@ -20,7 +20,7 @@ class UserAdmin(BaseUserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('username', 'email', 'password1', 'password2', 'is_admin', 'is_active')}
+            'fields': ('username', 'email', 'password1', 'password2', 'is_admin', 'is_active', 'tokens_used')}
         ),
     )
 
